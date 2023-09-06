63c8ad29a15fd7f3906c44b481b19ec968d8dd4f
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2023-09-06 15:20:44 +0100

    task-6

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import axios from "axios";
+import ChangeVotes from "./ChangeVotes"
 
 export default function SingleArticle({article}) {
 const [anArticle, setAnArticle] = useState({})
-
-
 const {article_id} = useParams()
 
 
@@ -17,26 +16,6 @@ useEffect(() => {
 
           });
       }, [article_id]);
-//optimistic rendering below
-/*
-const increaseVotes = (vote) => {
-    patchVotes(vote);
-    console.log(vote, "line 24")
-}
-//optimistic rendering below
-const decreaseVotes = (vote) => {
-    patchVotes(vote);
-    console.log(vote, "line 29")
-}
-
-const patchVotes = (vote) => {
-    axios
-    .patch(`https://be-nc-news-tnfa.onrender.com/api/articles/${article_id}`)
-    .then({data})
-    console.log(data, "line 36")
-    setAnArticle(data.article, { inc_votes : vote})
-}
-*/
   return (
     <article className="anArticle">
       <h1>Title: {anArticle.title}</h1>
@@ -45,8 +24,7 @@ const patchVotes = (vote) => {
       <p>Topic: {anArticle.topic}</p>
       <p>Time Created: {anArticle.created_at}</p>
       <p>Votes: {anArticle.votes}</p>
-      <button onClick={increaseVotes(+1)}>Add vote</button>
-      <button onClick={decreaseVotes(-1)}>Subtract vote</button>
+      <ChangeVotes />
       <p>Comment Count: {anArticle.comment_count}</p>
       <img className="articlePicture"
         src={anArticle.article_img_url}
